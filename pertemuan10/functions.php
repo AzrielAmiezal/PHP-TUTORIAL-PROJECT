38f04bf97f6bcd08ac0dbbbbbd51b852b9fdbbaa
AzrielAmiezal <aazwary0@gmail.com>
2020-06-20 14:46:54 +0800

    INSERT DATA

--- pertemuan10/functions.php
+++ pertemuan10/functions.php
@@ -23,3 +23,23 @@ function query($query)
 
     return $rows;
 }
+
+function tambah($data)
+{
+    $conn = connection();
+
+    $gambar = htmlspecialchars($data['gambar']);
+    $noMatriks = htmlspecialchars($data['noMatriks']);
+    $nama = htmlspecialchars($data['nama']);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+
+    $query = "INSERT INTO
+                mahasiswa
+                VALUES
+                (null, '$gambar', '$noMatriks', '$nama', '$email', '$jurusan')
+                ";
+    mysqli_query($conn, $query);
+    echo mysqli_error($conn);
+    return mysqli_affected_rows($conn);
+}
